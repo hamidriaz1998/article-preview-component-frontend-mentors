8f298efca2c90637f323b16c2ab6f600a6252dbd
hamidriaz1998 <111028733+hamidriaz1998@users.noreply.github.com>
2024-08-30 17:12:36 +0500

    Add overlay

---
+++ script.js
@@ -0,0 +1,5 @@
+let button = document.querySelector("button");
+button.addEventListener("click", (e) => {
+  let overlay = document.querySelector(".share-overlay");
+  overlay.classList.toggle("active");
+});
